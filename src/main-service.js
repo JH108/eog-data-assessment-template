df9f4a963c315c79f5585a49a393e1a8141b0157
Jesse Hill <jsnahill@gmail.com>
2020-09-14 14:48:28 -0500

    Build out prompts for the main service

--- src/main-service.js
+++ src/main-service.js
@@ -1,7 +1,39 @@
 require("dotenv/config");
 /**
  * Your main service should implement CRUD operations on the postgres database
- * for the users table.
+ * for the users table. There are some simple route definitions listed as examples
+ * to help you get started. Feel free to deviate from the examples if you wish.
  *
  * Make sure to handle any errors that could arise as well!
+ */
+
+/**
+ * method: GET
+ * route: /users
+ * purpose: Return an array of users
+ * bonus: Support pagination
+ */
+
+/**
+ * method: GET
+ * route: /fullnames
+ * purpose: Return an array of the first and last name for each user
+ */
+
+/**
+ * method: POST
+ * route: /users
+ * purpose: Add a new user to the database
+ */
+
+/**
+ * method: PATCH
+ * route: /users
+ * purpose: Update a user record
+ */
+
+/**
+ * method: DELETE
+ * route: /users
+ * purpose: Remove a given user
  */
